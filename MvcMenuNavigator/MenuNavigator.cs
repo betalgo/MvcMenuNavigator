5b9a2db7f3cec7ca6820942f9ce788db9b1b6472
Tolga Kayhan <kayhantolga@hotmail.com>
2017-08-23 12:27:45 +0300

    tests

---
+++ MvcMenuNavigator/MenuNavigator.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using MvcMenuNavigator.NavigationMenu;
+
+namespace MvcMenuNavigator
+{
+    public class MenuNavigator : ActionFilterAttribute, IActionFilter
+    {
+        private readonly HeaderTop _headerTop;
+        private readonly HeaderSub _headerSub;
+
+        public MenuNavigator(HeaderTop headerTop, HeaderSub headerSub)
+        {
+            _headerTop = headerTop;
+            _headerSub = headerSub;
+        }
+
+        void IActionFilter.OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            filterContext.Controller.TempData["MvcMenuNavigator.HeaderTop"] = _headerTop;
+            filterContext.Controller.TempData["MvcMenuNavigator.HeaderSub"] = _headerSub;
+
+            OnActionExecuting(filterContext);
+        }
+    }
+}
---
+++ MvcMenuNavigator/PageData.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+using MvcMenuNavigator.NavigationMenu;
+
+namespace MvcMenuNavigator
+{
+    static class MvcNavigatorPageData
+    {
+        public static HeaderTop HeaderTop => (HeaderTop) HttpContext.Current.Session["MvcMenuNavigator.HeaderTop"];
+        public static HeaderTop HeaderSub => (HeaderTop) HttpContext.Current.Session["MvcMenuNavigator.HeaderSub"];
+    }
+}
